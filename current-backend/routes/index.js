89516fba6c9d9587a3b4146e0f1dfbf8e6a04063
Henry Jacobs <henryjacobscode@gmail.com>
2023-06-14 15:09:33 -0400

    Commit misc file deletes

--- current-backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router() 
-const index_controller = require("../controllers/index-controller")
-
-router.post("/make-new-feature", index_controller.POST_make_new_feature)
-
-
-module.exports = router
