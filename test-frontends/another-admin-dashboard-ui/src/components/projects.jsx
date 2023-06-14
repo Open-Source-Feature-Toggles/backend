0af85c94b90144341e3ed7df53dc4a368100a16e
Henry Jacobs <henryjacobscode@gmail.com>
2023-06-14 15:08:32 -0400

    Commit misc work on frontends

--- test-frontends/another-admin-dashboard-ui/src/components/projects.jsx
+++ test-frontends/another-admin-dashboard-ui/src/components/projects.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import styles from "../styles/new-project.module.css"
 
 function Projects(){
     
+    const projectNameInput = useRef()
+
+
+    const makeProject = async function (e) {
+        e.preventDefault()
+        let name = projectNameInput.current.value 
+        if (name.trim() === ''){ return }
+        let data = { 
+            name, 
+            features : [], 
+        }
+        let post_request = await fetch("http://localhost:3000/")
+
+    }
+
+
     return (
         <div>
-            Hi
+            <form className={styles.form} onSubmit={makeProject}>
+                <div className={styles.form_cell}>
+                    <label htmlFor="project-name">Project Name </label>
+                    <input required type="text" ref={projectNameInput}  />
+                </div>
+                <button className={styles.btn}>Make Project</button>
+            </form>
         </div>
     )
 }
