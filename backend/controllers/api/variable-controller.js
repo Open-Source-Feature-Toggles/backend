8d5c208b8180199bd68376c6ad592bbf0cea9686
Henry Jacobs <henryjacobscode@gmail.com>
2023-06-22 12:40:34 -0400

    Add update route to update variables

--- backend/controllers/api/variable-controller.js
+++ backend/controllers/api/variable-controller.js
@@ -15,11 +15,14 @@ function findParentFeatureQuery (name, owner, parentProject) {
     })
 } 
 
-function findVariableQuery ( name, parentFeature ) {
+function findVariableQuery ( name, owner, parentFeature ) {
     return Variable.findOne({
         $and : [
-            { name }, 
-            { parentFeatureName : parentFeature }, 
+            { $and : [
+                { name }, 
+                { parentFeatureName: parentFeature }
+            ]}, 
+            { owner }
         ]
     })
 }
@@ -46,7 +49,7 @@ exports.POST_make_new_variable = [
             } = req.body
             let [ findParentFeature, checkIfVariableExists ] = await Promise.all([
                 findParentFeatureQuery(parentFeature, username, parentProject),
-                findVariableQuery(name, parentFeature)
+                findVariableQuery(name, username, parentFeature)
             ])
             if (!findParentFeature){
                 return res.status(404).json({ errors: "could-not-find-parent-feature" })
@@ -95,7 +98,7 @@ exports.POST_delete_variable = [
             } = req.body
             let [ findParentFeature, checkIfVariableExists ] = await Promise.all([
                 findParentFeatureQuery(parentFeature, username, parentProject),
-                findVariableQuery(name, parentFeature)
+                findVariableQuery(name, username, parentFeature)
             ])
             if (!findParentFeature){
                 return res.status(409).json({ errors: "cant-find-project" })
@@ -120,35 +123,31 @@ exports.POST_delete_variable = [
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-We need to be able to 
-
-    1. Create a variable
-
-    2. Destroy a variable
-
-    3. Update a variable's online status
-
-    4. Other simple crud operations like update its name 
-
-*/ 
+exports.POST_update_variable_status = [
+    body("name").trim().notEmpty().escape(), 
+    body("parentFeature").trim().notEmpty().escape(), 
+    body("username").trim().notEmpty().escape(),
+    async function (req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()){
+                return res.status(400).json({ errors: errors.array() })
+            }
+            let { 
+                name, 
+                parentFeature, 
+                username, 
+            } = req.body 
+            let getVariable = await findVariableQuery(name, username, parentFeature).exec()
+            if (!getVariable){
+                res.status(404).json({ errors: "variable-not-found" })
+            }
+            getVariable.active = !getVariable.active
+            await getVariable.save()
+            res.status(200).json({ active_status : getVariable.active })
+        } catch (error) {
+            console.error(error)
+            res.sendStatus(500)
+        }
+    } 
+]
--- backend/routes/api/variables.js
+++ backend/routes/api/variables.js
@@ -4,6 +4,8 @@ const variable_controller = require('../../controllers/api/variable-controller')
 
 router.post("/make-new-variable", variable_controller.POST_make_new_variable)
 
+router.post("/update-variable-status", variable_controller.POST_update_variable_status)
+
 router.delete("/delete-variable", variable_controller.POST_delete_variable)
 
 module.exports = router
