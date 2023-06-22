cebe4cb1e1ee97c1a1f4022172b657b6ad94c336
Henry Jacobs <henryjacobscode@gmail.com>
2023-06-22 09:09:51 -0400

    change POST requests for deletes to DELETE

--- backend/routes/api/variables.js
+++ backend/routes/api/variables.js
@@ -4,4 +4,6 @@ const variable_controller = require('../../controllers/api/variable-controller')
 
 router.post("/make-new-variable", variable_controller.POST_make_new_variable)
 
+router.delete("/delete-variable", variable_controller.POST_delete_variable)
+
 module.exports = router
