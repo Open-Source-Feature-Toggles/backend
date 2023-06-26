078f7339b082494c07c5ea9be8a73b2779fccf61
Henry Jacobs <henryjacobscode@gmail.com>
2023-06-26 11:32:08 -0400

    Add signup controller + route to main program

---
+++ backend/controllers/auth/Signup.js
@@ -0,0 +1,53 @@
+const { body, validationResult } = require('express-validator')
+const User = require('../../models/auth/user')
+const { hash } = require('bcrypt')
+const { generateAccessToken, generateRefreshToken } = require('./utils/generateTokens')
+ 
+
+exports.signUp = [
+    body("username")
+        .trim()
+        .isLength({ min : 8 }).withMessage("Username must be 8 characters long")
+        .escape(),
+    body("password")
+        .trim()
+        .isLength({ min : 8 }).withMessage("Password must be 8 characters long"), 
+    body("confirm_password")
+        .trim()
+        .isLength({ min : 8 }).withMessage("Password must be 8 characters long"), 
+    async function (req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()){
+                return res.status(400).json({ errors : errors.array() })
+            }
+            let { 
+                username, 
+                password, 
+                confirm_password
+            } = req.body
+            let userExists = await User.findOne({ username }).exec()
+            if (userExists){
+                return res.status(409).json({ errors : "User with that username already exists" })
+            }
+            if (password !== confirm_password){
+                return res.status(409).json({ errors: "Password and Confirm Passowrd must match" })
+            }
+            let hashed_password = await hash(password, 10) 
+            let newUser = new User({
+                username,
+                password : hashed_password, 
+                created : new Date(), 
+            })
+            let accessToken = generateAccessToken(newUser.username)
+            let refreshToken = generateRefreshToken(newUser.username)
+            newUser.refreshToken = refreshToken 
+            await newUser.save()
+            res.cookie('rjid', `${refreshToken}`, { maxAge: 604800000, httpOnly: true, secure: true })
+            res.status(200).json({ accessToken })
+        } catch (error){
+            console.error(error)
+            res.sendStatus(500)
+        }
+    }
+]
