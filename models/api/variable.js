48c1fc2ce371be0146d79e8431f0217eb8b4900b
Henry Jacobs <henryjacobscode@gmail.com>
2023-07-10 11:50:39 -0400

    Add production/development specifiers for variable

--- models/api/variable.js
+++ models/api/variable.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema
 
 const variableSchema = new Schema({
     name : { type: Schema.Types.String, require: true }, 
-    active : { type : Schema.Types.Boolean, require : true}, 
     parentFeatureName: { type: Schema.Types.String, require: true },
     parentFeatureID : { type: Schema.Types.ObjectId, require: true },  
+    developmentEnabled : { type: Schema.Types.Boolean, require : true, default : false }, 
+    productionEnabled : { type: Schema.Types.Boolean, require : true, default : false }, 
     owner: { type: Schema.Types.String, require: true },  
 }, { timestamps : true })
 
