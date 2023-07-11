fbbdfd78ebf9974a2cce5ee7d4fdfefa947d165f
Hankus Pankus <henryjacobscode@gmail.com>
2023-07-10 22:48:03 -0400

    Restructure GetPayload with Psuedo Code

--- controllers/api/payload.js
+++ controllers/api/payload.js
@@ -3,6 +3,27 @@ const { getApiHeaders } = require('../../helpers/Api-Key-Helpers')
 const { SearchCache } = require('../../helpers/caching/Cache-Helpers')
 const { CachedResourceValid } = require('../../helpers/http-responses/Success-Messages')
 
+/* 
+
+REORDER OF GETPAYLOAD(REQ, RES) 
+        
+        let apiKey = getApiHeaders(req)
+        let payload = await SearchCache(apiKey)
+        let client_last_updated = Number(req.query.last_updated)
+        if (payload?.last_updated === client_last_updated){
+            return CachedResourceValid(res)
+        }
+        else if (payload) {
+            return res.json(payload)
+        }
+        else {
+            buildPayload for given apiKey 
+                if apiKey does not map to any project, return error 
+            save 'on the fly build to redis' 
+            send payload 
+        }
+*/
+
 async function GetPayload (req, res) {
     try {
         let apiKey = getApiHeaders(req)
