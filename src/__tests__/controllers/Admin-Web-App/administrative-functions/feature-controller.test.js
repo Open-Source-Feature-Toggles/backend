17ffd403008d50648f415c6622d4a891cc79ffc8
Henry Jacobs <henryjacobscode@gmail.com>
2023-07-27 13:44:06 -0400

    Cleanup and add comments to feature-controller tests

--- src/__tests__/controllers/Admin-Web-App/administrative-functions/feature-controller.test.js
+++ src/__tests__/controllers/Admin-Web-App/administrative-functions/feature-controller.test.js
@@ -7,7 +7,6 @@ const {
 const {
     makeVariable, 
     makeUserProjectAndFeature, 
-    makeUser, 
 } = require('../../../testDataGenerators')
 const Feature = require('../../../../models/api/feature')
 const Project = require('../../../../models/api/project')
@@ -54,13 +53,12 @@ describe('Tests ChangeProductionStatus function in Feature Controller', () => {
 
     describe('ChangeProductionStatus - Success Cases', () => {
 
-        it('Successfully calls /features/change-production-status and returns a 200', async () => {
-            await feature.ChangeProductionStatus()
-            let changeProductionResponse = feature.responses.ChangeProductionStatus
-            expect(changeProductionResponse.status).toBe(200)
+        it('Calls ChangeProductionStatus and returns a 200', async () => {
+            let response = await feature.ChangeProductionStatus()
+            expect(response.status).toBe(200)
         })
     
-        it('Correctly changes productionEnabled on feature to true', async () => {
+        it('Checks that feature.productionEnabled was changed to true', async () => {
             let beforeChange = await Feature.findOne({ name : goodOptions.featureName }) 
             await feature.ChangeProductionStatus()
             let afterChange = await Feature.findOne({ name : goodOptions.featureName }) 
@@ -71,24 +69,9 @@ describe('Tests ChangeProductionStatus function in Feature Controller', () => {
     
     describe('ChangeProductionStatus - Error Cases', () => {
 
-        it('It calls ChangeProductionStatus with an incorrect feature name and returns a 404', async () => {
-            let response = await request(app)
-                .post('/features/change-production-status')
-                .set('Cookie', user.getFullCookie())
-                .send({
-                    featureName : 'wrong-name', 
-                    projectName : goodOptions.projectName
-                })
-            expect(response.status).toBe(404)
-        })
-        it('It calls ChangeProductionStatus with an incorrect project name and returns a 404', async () => {
-            let response = await request(app)
-                .post('/features/change-production-status')
-                .set('Cookie', user.getFullCookie())
-                .send({
-                    featureName : goodOptions.featureName, 
-                    projectName : 'wrong-name'
-                })
+        it('Calls ChangeProductionStatus on a nonexistent feature and returns a 404', async () => {
+            await feature.DeleteFeature()
+            let response = await feature.ChangeProductionStatus()
             expect(response.status).toBe(404)
         })
     })
@@ -99,12 +82,11 @@ describe('Tests ChangeDevelopmentStatus function in Feature Controller', () => {
 
     describe('ChangeDevelopmentStatus - Success Cases', () => {
 
-        it('Successfully calls /features/change-development-status and returns a 200', async () => {
-            await feature.ChangeDevelopmentStatus()
-            let changeDevelopmentResponse = feature.responses.ChangeDevelopmentStatus
-            expect(changeDevelopmentResponse.status).toBe(200)
+        it('Calls ChangeDevelopmentStatus and returns a 200', async () => {
+            let response = await feature.ChangeDevelopmentStatus()
+            expect(response.status).toBe(200)
         })
-        it('Correctly changes developmentEnabled on feature to true', async () => {
+        it('Checks that feature.developmentEnabled was changed to true', async () => {
             let beforeChange = await Feature.findOne({ name : goodOptions.featureName }) 
             await feature.ChangeDevelopmentStatus()
             let afterChange = await Feature.findOne({ name : goodOptions.featureName }) 
@@ -115,24 +97,9 @@ describe('Tests ChangeDevelopmentStatus function in Feature Controller', () => {
 
     describe('ChangeDevelopmentStatus - Error Cases', () => {
 
-        it('It calls ChangeDevelopmentStatus with an incorrect feature name and returns a 404', async () => {
-            let response = await request(app)
-                .post('/features/change-development-status')
-                .set('Cookie', user.getFullCookie())
-                .send({
-                    featureName : 'wrong-name', 
-                    projectName : goodOptions.projectName
-                })
-            expect(response.status).toBe(404)
-        })
-        it('It calls ChangeProductionStatus with an incorrect project name and returns a 404', async () => {
-            let response = await request(app)
-                .post('/features/change-development-status')
-                .set('Cookie', user.getFullCookie())
-                .send({
-                    featureName : goodOptions.featureName, 
-                    projectName : 'wrong-name'
-                })
+        it('It calls ChangeDevelopmentStatus on a nonexistent feature and returns a 404', async () => {
+            await feature.DeleteFeature()
+            let response = await feature.ChangeDevelopmentStatus()
             expect(response.status).toBe(404)
         })
     })
@@ -142,10 +109,9 @@ describe('Tests DeleteFeature function in Feature Controller', () => {
 
     describe('DeleteFeature - Success Cases', () => {
 
-        it('Successfully calls features/delete-feature and returns a 200', async () => {
-            await feature.DeleteFeature()
-            let deleteFeatureResponse = feature.responses.DeleteFeature
-            expect(deleteFeatureResponse.status).toBe(200)
+        it('Successfully calls DeleteFeature and returns a 200', async () => {
+            let response = await feature.DeleteFeature()
+            expect(response.status).toBe(200)
         })
         it('Successfully deletes a feature', async () => {
             let beforeChange = await Feature.findOne({ name : goodOptions.featureName })
@@ -155,17 +121,29 @@ describe('Tests DeleteFeature function in Feature Controller', () => {
             expect(afterChange).toBe(null)
         })
         it('Successfully removes feature from the project\'s feature array', async () => {
-            let getFeature = await Feature.findOne({ name : goodOptions.featureName })
+            /* 
+            * We first need to get the id of the feature we'll be testing
+            * We then query the project before we delete the feature to make
+            * sure it lists the feature as one of its children. 
+            * We then perform the delete and repeat the last two steps to ensure 
+            * the feature was removed from the project's array 
+            */  
+            let featureID = (await Feature.findOne({ name : goodOptions.featureName }))._id
             let projectBeforeDelete = await Project.findOne({ name : goodOptions.projectName })
-            let isFeatureIdinProjectArray = projectBeforeDelete.features.includes(getFeature._id)
+            let isFeatureIdinProjectArray = projectBeforeDelete.features.includes(featureID)
             await feature.DeleteFeature()
             let projectAfterDelete = await Project.findOne({ name : goodOptions.projectName })
-            let isFeatureIdNotInProjectArray = projectAfterDelete.features.includes(getFeature._id)
+            let isFeatureIdNotInProjectArray = projectAfterDelete.features.includes(featureID)
             expect(isFeatureIdinProjectArray).toBe(true)
             expect(isFeatureIdNotInProjectArray).toBe(false)
         })
         it('Successfully deletes all of the feature\'s children variables', async () => {
-            let newVariable = await makeVariable({
+            /* 
+            * We first make a new variable, so that there are two variables  
+            * in the feature's array. This is done to test that all of a 
+            * feature's variables are deleted, not just one. 
+            */
+            await makeVariable({
                 app : app, 
                 fakeUser : user, 
                 fakeProject : project, 
@@ -184,24 +162,22 @@ describe('Tests DeleteFeature function in Feature Controller', () => {
 
     describe('DeleteFeature - Error Cases', () => {
 
-        it('Attempts to call DeleteFeature on a nonexistent feature and returns a 404', async () => {
-            let response = await request(app)
-                .delete('/features/delete-feature')
-                .set('Cookie', user.getFullCookie())
-                .send({
-                    featureName : 'incorrect-name', 
-                    projectName : goodOptions.projectName, 
-                })
+        it('Calls DeleteFeature on a nonexistent feature and returns a 404', async () => {
+            /* 
+            * We first successfully delete the feature, so when we try a second time
+            * It mimics trying to delete a feature that DNE 
+            */
+            await feature.DeleteFeature()
+            let response = await feature.DeleteFeature()
             expect(response.status).toBe(404)
         })
-        it('Attempts to call DeleteFeature on a feature with a nonexistent project and returns a 404', async () => {
-            let response = await request(app)
-                .delete('/features/delete-feature')
-                .set('Cookie', user.getFullCookie())
-                .send({
-                    featureName : goodOptions.featureName, 
-                    projectName : 'incorrect-name', 
-                })
+        it('Calls DeleteFeature on a nonexistent project and returns a 404', async () => {
+            /* 
+            * First delete the project so that the controller isn't able to find a 
+            * a valid project associated with the given variable 
+            */
+            await project.DeleteFakeProject()
+            let response = await feature.DeleteFeature()
             expect(response.status).toBe(404)
         })
     })
@@ -209,71 +185,62 @@ describe('Tests DeleteFeature function in Feature Controller', () => {
 
 describe('Tests MakeNewFeature function in Feature Controller', () => {
 
-    // This particular block is testing to see if we can create 
-    // a user, and therefore needs to start with no data 
-    beforeEach( async () => {
-        await clearDatabase()
-    })
-
     describe('MakeNewFeature - Success Cases', () => {
 
-        it('Calls /features/make-new-feature and returns a status of 200', async () => {
-            let { fakeFeature }  = await makeUserProjectAndFeature(goodOptions)
-            let createNewFeatureStatus = fakeFeature.responses.CreateFakeFeature
-            expect(createNewFeatureStatus.status).toBe(200) 
+        it('Calls MakeNewFeature and returns a status of 200', async () => {
+            let response = feature.retrieveResponse('CreateFakeFeature')
+            expect(response.status).toBe(200)
         })
         it('Successfully creates a new feature', async () => {
-            let numFeaturesBeforeCreation = await Feature.find()
-            let { fakeFeature } = await makeUserProjectAndFeature(goodOptions)
-            let numFeaturesAfterCreation = await Feature.find()
-            expect(numFeaturesBeforeCreation.length).toBe(0)
-            expect(numFeaturesAfterCreation.length).toBe(1)
+            let findFeature = await Feature.findOne({ name : goodOptions.featureName })
+            expect(findFeature).not.toBe(null)
         })
-        it('Successfully creates a child variable by default', async () => {
-            let { fakeFeature } = await makeUserProjectAndFeature(goodOptions)
-            let findChildVariable = await Variable.findOne({ name: goodOptions.featureVariableName })
-            expect(findChildVariable).not.toBe(null)
+        it('Successfully creates a child variable and adds it ', async () => {
+            let [findChildVariable, findFeature ] = await Promise.all([
+                Variable.findOne({ parentFeatureName : goodOptions.featureName }), 
+                Feature.findOne({ name : goodOptions.featureName })
+            ]) 
+            let variableID = findChildVariable._id
+            expect(findFeature.variables.includes(variableID)).toBe(true)
         })
         it('Successfully adds new feature\'s id to project\'s feature array', async () => {
-            let { fakeFeature } = await makeUserProjectAndFeature(goodOptions)
-            let findFeature = await Feature.findOne({ name: goodOptions.featureName })
-            let findProject = await Project.findOne({ name : goodOptions.projectName })
-            let featureIdFromProjectArray = findProject.features[0]
-            expect(findProject.features.length).toBe(1)
-            expect(featureIdFromProjectArray).toEqual(findFeature._id)
+            let [ findFeature, findParentProject ] = await Promise.all([
+                Feature.findOne({ name: goodOptions.featureName }), 
+                Project.findOne({ name : goodOptions.projectName })
+            ])
+            let featureID = findFeature._id
+            expect(findParentProject.features.includes(featureID)).toBe(true)
         })
     })
 
     describe('MakeNewFeature - Error Cases', () => {
 
-        it('Tries to create a feature with the same name as an existing feature on the same project', async () => {
-            let { fakeFeature, fakeUser } = await makeUserProjectAndFeature(goodOptions)
-            let makeNewFeatureResponse = await request(app)
-                .post('/features/make-new-feature')
-                .set('Cookie', fakeUser.getFullCookie())
-                .send({
-                    featureName : goodOptions.featureName, 
-                    description : 'some-description', 
-                    initialVariableKey : goodOptions.initialVariableKey, 
-                    featureVariableName : goodOptions.featureVariableName, 
-                    projectName: goodOptions.projectName
-                })
-            expect(makeNewFeatureResponse.status).toBe(409)
+        it('Tries to create a feature with a name/owner that already exists and returns a 409', async () => {
+            /* 
+            * At the beginning of the script, we call CreateFakeFeature with
+            * a feature name that's taken from the goodOptions object. Here, 
+            * we try to create the same feature again with the same options (same owner, project,
+            * and feature name) as the one that was initially created. 
+            */ 
+            let response = await feature.CreateFakeFeature()
+            expect(response.status).toBe(409)
         })
-        // it('Tries to create a feature for a nonexistent project', async () => {
-        //     let { fakeUser } = await makeUser(app, USERNAME, PASSWORD) 
-        //     let makeNewFeatureResponse = await request(app)
-        //         .post('/features/make-new-feature')
-        //         .set('Cookie', fakeUser.getFullCookie())
-        //         .send({
-        //             featureName : goodOptions.featureName, 
-        //             description : 'some-description', 
-        //             initialVariableKey : goodOptions.initialVariableKey, 
-        //             featureVariableName : goodOptions.featureVariableName, 
-        //             projectName: 'not-an-actual-project'
-        //         })         
-        //     expect(makeNewFeatureResponse.status).toBe(409)
-        // }) 
-        //          This doesnt work ^^ 
+        it('Tries to create a feature for a nonexistent project', async () => {
+            /* 
+            * We first call DeleteFakeProject on our project that was created before 
+            * this test. When we make a call to the DeleteProject controller, we 
+            * delete all of the associated variables and features with that project
+            * along with the project itself. This is done in the beginning of the test
+            * so that when we call CreateFakeFeature, the controller is unable to find
+            * a feature that exists with that name (which is what we want for this test),
+            * but is unable to find a project with the given name/owner, which is what
+            * this is testing. 
+            */
+            await Promise.all([
+                project.DeleteFakeProject(), 
+            ])
+            let response = await feature.CreateFakeFeature()
+            expect(response.status).toBe(404)
+        })  
     })
 })
