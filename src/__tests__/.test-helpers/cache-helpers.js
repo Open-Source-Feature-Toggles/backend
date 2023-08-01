90faa189c36e50f14bb2728e3b026438780b6827
Henry Jacobs <henryjacobscode@gmail.com>
2023-08-01 15:53:27 -0400

    Write delayed cache helpers that allow the cache to updateD

--- src/__tests__/.test-helpers/cache-helpers.js
+++ src/__tests__/.test-helpers/cache-helpers.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
+const client = require('../../config/redis.config')
 
-
-async function getPayload (app, apiKey) {
+async function requestPayload (app, apiKey) {
     let response = await request(app)
         .get('/api/payload')
         .set('Authorization', apiKey)
@@ -9,10 +9,50 @@ async function getPayload (app, apiKey) {
 }
 
 function getVariableFromCache (cache, feature, variable) {
-    return cache?.features.feature?.variable
+    let feature_exists = cache?.['features'][feature] 
+    if (feature_exists){
+        for (let entry of feature_exists){
+            if (entry.hasOwnProperty(variable)){
+                return entry[variable]
+            }
+        }
+        return undefined
+    }
 }
+
+async function showKeys () {
+    return await client.sendCommand(['KEYS', '*'])
+}
+
+async function getOutputByKey (key) {
+    let string = await client.sendCommand(['GET', `${key}`])
+    return JSON.parse(string)
+}
+
+async function readCache (apiKey) {
+    return new Promise( (resolve) => {
+        setTimeout( async () => {
+            let cachedResult = await getOutputByKey(apiKey)
+            resolve(cachedResult)
+        }, 100);
+    })
+}
+
+async function getPayload (app, apiKey) {
+    return new Promise( (resolve) => {
+        setTimeout( async () => {
+            let payload = await requestPayload(app, apiKey)
+            resolve(payload)
+        }, 100)
+    })
+}
+
 
 module.exports = { 
     getPayload, 
-    getVariableFromCache
+    getVariableFromCache, 
+    showKeys, 
+    getOutputByKey, 
+    readCache,  
+    getPayload, 
 }
