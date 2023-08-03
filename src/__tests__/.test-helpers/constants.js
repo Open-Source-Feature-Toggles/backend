56879c83bde362aa5fb763895fad7f72e380e673
Henry Jacobs <henryjacobscode@gmail.com>
2023-08-03 11:53:07 -0400

    Create constants file to hold fakeData

---
+++ src/__tests__/.test-helpers/constants.js
@@ -0,0 +1,13 @@
+let fakeData = { 
+    username : 'fakeuser', 
+    password : 'fakepassword', 
+    projectName : 'fake-project', 
+    featureName : 'fake-feature', 
+    description : 'fake-description', 
+    initialVariableKey : 'variable-key', 
+    featureVariableName : 'variable-1', 
+    newVariableName : 'variable-2'
+}
+
+
+module.exports = fakeData
