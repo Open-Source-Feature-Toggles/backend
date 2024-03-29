09af3f50c850d0183cae39cabd6983e8ef26aaa9
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-03-29 08:43:10 -0700

    add custom project-logging

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ function CreateApp() {
   const cors = require('cors');
   const morgan = require('morgan');
   const cookie_parser = require('cookie-parser');
+  const chalk = require('chalk')
   const projectRoutes = require('./routes/Admin-Web-App/administrative/projects');
   const featuresRoutes = require('./routes/Admin-Web-App/administrative/features');
   const variableRoutes = require('./routes/Admin-Web-App/administrative/variables');
@@ -24,7 +25,20 @@ function CreateApp() {
       credentials: true,
     }),
   );
-  app.use(morgan('dev'));
+
+  morgan.token('color-status', function (req, res) {
+    const status = res.statusCode;
+
+    // Apply color based on the response status code range
+    if (status >= 500) return chalk.red(status);
+    if (status >= 400) return chalk.yellow(status);
+    if (status >= 300) return chalk.cyan(status);
+    if (status >= 200) return chalk.green(status);
+    return status; // Default to no color if none of the above
+  });
+
+
+  app.use(morgan(':date[web] :method :url :color-status :response-time ms - :res[content-length]'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookie_parser());
